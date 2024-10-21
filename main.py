c17c517f510d5a3a5f034dc04996b82019f7bb86
Dreeeems <114916033+Dreeeems@users.noreply.github.com>
2024-10-21 23:55:41 +0200

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import discord
 from dotenv import load_dotenv
 import random
 
-# URLs des champions et des runes
+
 
 version = "https://ddragon.leagueoflegends.com/api/versions.json"
 fetchinUrl = "https://ddragon.leagueoflegends.com/cdn/14.20.1/data/fr_FR/champion.json"
@@ -14,7 +14,7 @@ base_image_url = "https://ddragon.leagueoflegends.com/cdn/img/"
 def get_latest_version():
     versions = fetch(version)
     if versions:
-        return versions[0]  # Retourne la première version
+        return versions[0] 
     return None
 
 def fetch(url):
@@ -29,29 +29,29 @@ def searchRune(url):
     runesUrl = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/fr_FR/runesReforged.json"
     données = fetch(runesUrl)
     if données:
-        # Sélectionner un arbre principal et un arbre secondaire aléatoires
+   
         primary_tree = random.choice(données)
-        secondary_tree = random.choice([tree for tree in données if tree != primary_tree])  # S'assurer que ce n'est pas le même arbre
+        secondary_tree = random.choice([tree for tree in données if tree != primary_tree])  
 
-        # Sélectionner toutes les runes de chaque slot de l'arbre primaire
+      
         primary_runes = []
         for slot in primary_tree["slots"]:
-            primary_rune = random.choice(slot["runes"])  # Sélectionner une rune aléatoire par slot
+            primary_rune = random.choice(slot["runes"]) 
             primary_runes.append((primary_rune["name"]))
 
-        # Sélectionner toutes les runes de chaque slot de l'arbre secondaire (deux slots)
+ 
         secondary_runes = []
         secondary_runes = []
         available_runes = []
-        for slot in secondary_tree["slots"][-3:]:  # Limité aux trois derniers slots
-            available_runes.extend(slot["runes"])  # Ajouter toutes les runes disponibles
+        for slot in secondary_tree["slots"][-3:]:  
+            available_runes.extend(slot["runes"])  
 
-        # Sélectionner deux runes aléatoires parmi les runes disponibles
+
         selected_runes = random.sample(available_runes, 2)
         for rune in selected_runes:
             secondary_runes.append((rune["name"]))
 
-        # Retourner les informations des runes
+
         return {
             "primary_tree": primary_tree["name"],
             "primary_runes": primary_runes,
@@ -71,11 +71,11 @@ def searchChamp(url):
         return données["data"][champ]
     return None
 
-# Charger les variables d'environnement
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Initialiser le bot Discord
+
 intents = discord.Intents.all()
 intents.messages = True
 client = discord.Client(intents=intents)
